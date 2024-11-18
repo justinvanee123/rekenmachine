8d3bd21f565511343f3c6975c3132f8d22dbd699
justinvanee123 <justinvanee123@hotmail.com>
2024-11-18 08:49:26 +0100

    Initial commit

---
+++ rekenmachine/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Vraag de gebruiker om het eerste getal in te voeren
+        Console.WriteLine("Voer het eerste getal in:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        // Vraag de gebruiker om het tweede getal in te voeren
+        Console.WriteLine("Voer het tweede getal in:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        // Vraag de gebruiker welke bewerking hij/zij wil uitvoeren
+        Console.WriteLine("Welke bewerking wil je uitvoeren? (+, -, x, /)");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        // Uitvoeren van de gekozen bewerking
+        double result = 0;
+        switch (operation)
+        {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case 'x':
+                result = number1 * number2;
+                break;
+            case '/':
+                // Controleer deling door nul
+                if (number2 != 0)
+                    result = (double)number1 / number2;
+                else
+                    Console.WriteLine("Kan niet delen door nul.");
+                break;
+            default:
+                Console.WriteLine("Ongeldige bewerking.");
+                break;
+        }
+
+        // Toon het resultaat aan de gebruiker
+        if (operation != '/')
+            Console.WriteLine($"Het resultaat van {number1} {operation} {number2} is: {result}");
+
+        // Houd het programma open totdat de gebruiker een toets indrukt om af te sluiten
+        Console.WriteLine("Druk op een toets om af te sluiten...");
+        Console.ReadKey();
+    }
+}
